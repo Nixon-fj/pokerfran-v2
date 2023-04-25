1b1e25d062a43b76ada57d5889d8017544b01aa4
nixon <nichofernandez1@gmail.com>
2023-04-25 11:14:26 -0600

    correcio

--- playnow-master/README.md
+++
@@ -1,6 +0,0 @@
-# playnow
-
-usar la version de node de 14.17.5
-
-npm i
-
--- playnow-master/conf.php
+++
@@ -1,7 +0,0 @@
-<?php
-define("SITE_NAME","Playnow");
-define("DOMAIN_NAME","Playnow.ag");
-define("DGS_SITEID","846");
-
-require "C:\\inetpub\\wwwroot\\generalConfig.php";
-?>
