10deb1a4334439059745cfdca387d587be69180b
nixon <nichofernandez1@gmail.com>
2023-04-25 11:10:45 -0600

    cambio de generales

---
+++ playnow-master/README.md
@@ -0,0 +1,6 @@
+# playnow
+
+usar la version de node de 14.17.5
+
+npm i
+
---
+++ playnow-master/conf.php
@@ -0,0 +1,7 @@
+<?php
+define("SITE_NAME","Playnow");
+define("DOMAIN_NAME","Playnow.ag");
+define("DGS_SITEID","846");
+
+require "C:\\inetpub\\wwwroot\\generalConfig.php";
+?>
